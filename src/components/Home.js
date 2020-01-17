6c99bed2d01f627076c07d6324f5a024118f6229
Valiana <valiana.josic@gmail.com>
2020-01-17 17:16:09 -0500

    Added BrowserRouter, Route, Switch

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import git from '../git.svg';
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="nav-div">
+      <nav className="nav">
+          <ul>
+            <li><a href="index.html" className="nav-page current-page">Home</a></li>
+            <li><a href="projects.html" className="nav-page">Projects</a></li>   
+            <li><a href="contact.html" className="nav-page">Contact</a></li>   
+          </ul>
+      </nav>
+      </div>
+      <header className="App-header">
+      <a 
+        href="https://github.com/ValianaJosic" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        className="git-link App-logo" >
+        <img src={git} alt="github-logo" className="git-icon"/>
+        </a>
+        <p> 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
